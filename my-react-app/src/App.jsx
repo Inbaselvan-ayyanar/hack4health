ab1016bedd82774915e6a22a89b162c4169b22a9
Inbaselvan-ayyanar <141208152+Inbaselvan-ayyanar@users.noreply.github.com>
2025-07-15 18:07:39 +0530

    Initial commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import About from './pages/About'
+function App() {
+  return (
+    <div>
+      <h1>My App</h1>
+      <About />
+    </div>
+  )
+}
+
+export default App
